b12bff47fffbebffbbba0f53a37d0892fe2521f3
Rajeshyadav111 <97342862+Rajeshyadav111@users.noreply.github.com>
2022-01-08 18:29:46 +0530

    Add files via upload

---
+++ S.java
@@ -0,0 +1,16 @@
+//THIS IS VARIABLES EXAMPLE
+package corejava;
+
+public class S {
+	
+	public static void main(String[] args) {
+	 int i=10;
+	String g="SUCCESS";
+	double d=123.445;
+	
+	
+	
+		System.out.println("this is integer:"+i);
+	}
+
+}
